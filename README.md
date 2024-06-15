543b646d3a32e4027a839463fb07759ed71330e2
devRojha <devraj227804@gamil.com>
2024-06-15 15:47:37 +0530

    restart

--- README.md
+++
@@ -1,3 +0,0 @@
-# NITP, WEB TEAM
-Task-1 clone the front Page of webpage (Rollpark);
-Technology - React+vite, React-router-dom, AOS, React-icon, Tailwind, JS ;
--- src/components/CirculButton.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export function CirculButton({onClick, icon}){
-    return (
-        <button onClick={onClick} className='border h-[50px] w-[50px]  rounded-full bg-green-500 hover:bg-green-300 flex  justify-center'>
-            <img className="mt-3" src={icon} alt="L" />
-        </button>
-    )
-}
